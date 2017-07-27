421c47b5b893b4a5231ecd00b87b6a0d7abd5817
cainsyake <598147223@qq.com>
2017-07-27 11:30:41 +0800

    增加baseState模块的JPA接口

---
+++ src/main/java/bobo/erp/repository/state/BaseStateRepository.java
@@ -0,0 +1,11 @@
+package bobo.erp.repository.state;
+
+
+import bobo.erp.domain.state.BaseState;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by 59814 on 2017/7/27.
+ */
+public interface BaseStateRepository extends JpaRepository<BaseState, Integer> {
+}
