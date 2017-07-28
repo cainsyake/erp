22848e2059d29d7af890bb7a1f08e0e48def7a85
cainsyake <598147223@qq.com>
2017-07-28 14:21:06 +0800

    通过username查询某个subUserInfo

--- src/main/java/bobo/erp/repository/teach/SubUserInfoRepository.java
+++ src/main/java/bobo/erp/repository/teach/SubUserInfoRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
  * Created by 59814 on 2017/7/23.
  */
 public interface SubUserInfoRepository extends JpaRepository<SubUserInfo, Integer> {
+    public SubUserInfo findBySubUserName(String subUserName);
 }
