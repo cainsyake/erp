9023017454e5c5aef8996acb1243f71058f96f49
cainsyake <598147223@qq.com>
2017-11-28 08:22:09 +0800

    RuleProductBom

--- src/main/java/bobo/erp/entity/rule/RuleProductBom.java
+++ src/main/java/bobo/erp/entity/rule/RuleProductBom.java
@@ -1,5 +1,7 @@
 package bobo.erp.entity.rule;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -19,12 +21,54 @@ public class RuleProductBom {
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "bom_id")
-    private List<Integer> materialBomList;    //原料组成
+    private List<Integer> materialBomList;    //原料组成，List中依次放入原料1、原料2、...、原料n的消耗量
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "bom_id")
-    private List<Integer> productBomList;    //半成品组成
+    private List<Integer> productBomList;    //半成品组成，，List中依次放入产品1、产品2、...、产品n的消耗量
 
     @ManyToOne
     private Rule rule;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public List<Integer> getMaterialBomList() {
+        return materialBomList;
+    }
+
+    public void setMaterialBomList(List<Integer> materialBomList) {
+        this.materialBomList = materialBomList;
+    }
+
+    public List<Integer> getProductBomList() {
+        return productBomList;
+    }
+
+    public void setProductBomList(List<Integer> productBomList) {
+        this.productBomList = productBomList;
+    }
+
+    @JsonBackReference
+    public Rule getRule() {
+        return rule;
+    }
+
+    @JsonBackReference
+    public void setRule(Rule rule) {
+        this.rule = rule;
+    }
 }
