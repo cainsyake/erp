c9dba434aab30eeb23233cd2406953a326b7bfde
cainsyake <598147223@qq.com>
2017-12-09 17:11:12 +0800

    广告和运行状态的Packing Entity

---
+++ src/main/java/bobo/erp/entity/common/packing/runPack/RunAdPacking.java
@@ -0,0 +1,11 @@
+package bobo.erp.entity.common.packing.runPack;
+
+import bobo.erp.entity.state.marketing.AdvertisingState;
+
+/**
+ * Created by bobo on 2017/12/9.
+ */
+public class RunAdPacking {
+    public AdvertisingState advertisingState;
+    public String username;
+}
---
+++ src/main/java/bobo/erp/entity/common/packing/runPack/RunningStatePacking.java
@@ -0,0 +1,14 @@
+package bobo.erp.entity.common.packing.runPack;
+
+import bobo.erp.entity.state.RunningState;
+
+import java.util.List;
+
+
+/**
+ * Created by bobo on 2017/12/9.
+ */
+public class RunningStatePacking {
+    public RunningState runningState;
+    public List<Integer> meetingStateList;
+}
